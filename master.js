beb2ec51ebaf06754c2f0d6289133ee69ce49615
victoria-parker <victoriaparkerb@gmail.com>
2021-11-30 18:58:16 +0000

    delete button done and renderLeads improved

--- master.js
+++ master.js
@@ -2,24 +2,31 @@ let myLeads=[]
 const inputEl=document.getElementById("input-el")
 const inputBtn=document.getElementById("input-btn")
 const ulEl=document.getElementById("ul-el")
+const deleteBtn=document.getElementById("delete-btn")
 let leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"))
 
 if(leadsFromLocalStorage){
     myLeads=leadsFromLocalStorage;
-    renderLeads()
+    renderLeads(myLeads)
 }
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value) 
     inputEl.value=""
     localStorage.setItem("myLeads",JSON.stringify(myLeads))
-    renderLeads()
+    renderLeads(myLeads)
 })
 
-function renderLeads(){
+function renderLeads(leads){
     let listItem=""
-    for(let i=0;i<myLeads.length;i++){
-    listItem+=`<li><a href="${myLeads[i]}">${myLeads[i]}</a></li>`
+    for(let i=0;i<leads.length;i++){
+    listItem+=`<li><a href="${leads[i]}">${leads[i]}</a></li>`
     }
     ulEl.innerHTML= listItem
 }
+
+deleteBtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads=[]
+    renderLeads(myLeads)
+})
