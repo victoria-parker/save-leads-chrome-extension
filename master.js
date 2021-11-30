03a217a8324b536b3e7219777ccee744a0b0d818
victoria-parker <victoriaparkerb@gmail.com>
2021-11-30 18:49:07 +0000

    save leads in localStorage and render them in the list if any

--- master.js
+++ master.js
@@ -1,11 +1,18 @@
-let myLeads=['algo','alog','gola']
+let myLeads=[]
 const inputEl=document.getElementById("input-el")
 const inputBtn=document.getElementById("input-btn")
 const ulEl=document.getElementById("ul-el")
+let leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"))
+
+if(leadsFromLocalStorage){
+    myLeads=leadsFromLocalStorage;
+    renderLeads()
+}
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value) 
     inputEl.value=""
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
     renderLeads()
 })
 
